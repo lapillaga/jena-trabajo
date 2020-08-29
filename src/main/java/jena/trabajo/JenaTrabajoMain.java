be2a3def71cde5e11de1c356bdc940d7a8a32eda
Luis Pillaga <lapillaga@espe.edu.ec>
2020-08-28 22:08:48 -0500

    ADD: Primer queries funcionando

--- src/main/java/jena/trabajo/JenaTrabajoMain.java
+++ src/main/java/jena/trabajo/JenaTrabajoMain.java
@@ -10,33 +10,32 @@ import java.io.InputStream;
 
 public class JenaTrabajoMain {
     public static void main(String[] args) {
-        OntModel model=null;
+        OntModel model = null;
         model = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM);
-        InputStream in= FileManagerImpl.get().open("/Users/luispillaga/Downloads/proyecto_final_matriculacion.owl");
-        if (in==null)
-        {
+        InputStream in = FileManagerImpl.get().open("/Users/luispillaga/Downloads/proyecto_final_matriculacion.owl");
+        if (in == null) {
             throw new IllegalArgumentException("Archivo no encontrado");
         }
         model.read(in, "");
-        model.write(System.out);
-//        String queryString= "PREFIX hc: <http://www.semanticweb.org/pc/ontologies/2020/6/untitled-ontology-5#>" +
-//                "SELECT ?transaccion ?cajero" +
-//                "WHERE "+
-//                "{"+
-//                "?t  hc:GeneradaPorCajero ?c."+
-//                "?t  hc:codigo ?transaccion."+
-//                "?c  hc:direccion ?cajero."+
-//                "}";
-//        Query query= QueryFactory.create(queryString);
-//        QueryExecution qu= QueryExecutionFactory.create(query,model);
-//
-//        try{
-//            ResultSet results;
-//            results=(ResultSet)qu.execSelect();
-//            ResultSetFormatter.out(System.out,results, query);
-//        }finally {
-//            qu.close();
-//        }
+//        model.write(System.out);
+        String queryString = "PREFIX ma: <http://www.semanticweb.org/pc/ontologies/2020/7/untitled-ontology-10#>" +
+            "SELECT ?estudiante ?materia " +
+            "WHERE " +
+            "{" +
+                "?m ma:TieneMaterias ?mat." +
+                "?mat ma:nombre ?materia." +
+                "?e ma:MatriculadoEn ?m." +
+                "?e ma:nombre ?estudiante." +
+            "}";
+        Query query = QueryFactory.create(queryString);
+        QueryExecution qu = QueryExecutionFactory.create(query, model);
+        try {
+            ResultSet results;
+            results = qu.execSelect();
+            ResultSetFormatter.out(System.out, results, query);
+        } finally {
+            qu.close();
+        }
 
     }
 }
